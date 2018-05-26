3bc9ecb206577a8602d401f34b28794a678e4b4d
mwall <mwall@iastate.edu>
2018-05-26 17:29:35 -0500

    cleaned up msgs

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ client.on("message", async message => {
     const command = args.shift().toLowerCase();
 
     if(command === "build") {
+
+        //clear the command
+        const fetched = await message.channel.fetchMessages({count: 1});
+        message.delete(fetched)
+            .catch(error => message.send(`Couldn't delete messages because of: ${error}`));
+
         args.forEach(link => {
 
             link_builder.build(link, userAgent, (err, title, links, img, color) => {
